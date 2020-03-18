6438275925c2841f6bad817ba6071d20fd498113
earl86 <earl86@users.noreply.github.com>
2020-03-18 13:10:42 +0800

    add for python3 version

---
+++ mysql/scripts/epmmm_get_mysql_stats_wrapper-python3.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# The wrapper for zabbix python3 script.
+# It runs the script every 30 seconds. and parses the cache file on each following run.
+# Test the script using: zabbix_get -s 192.168.1.200 -p 10050 -k "epmmm.mysql.check[your_mysqlservice_hostname,192.168.1.200,3306,check]"
+# The script need zabbix_sender and Python Running Environment
+# The default monitor mysql port is 3306, if need to monitor other port, you need to add the Macros:{$MYSQL_PORT}=xxxx on the monitor Host
+# Remember to change the USERNAME PASSWORD for your's.
+# Authors: earl86
+
+USERNAME=zabbix
+PASSWORD=zabbix
+
+
+SERVICEHOSTNAME=$1
+SERVICEIP=$2
+SERVICEPORT=$3
+ITEM=$4
+
+DIR=`dirname $0`
+
+CMD="/usr/bin/python3 $DIR/epmmm_get_mysql_stats-python3.py --servicehostname $SERVICEHOSTNAME --serviceip $SERVICEIP --serviceport $SERVICEPORT --username $USERNAME --password $PASSWORD"
+
+if [ "$ITEM" = "mysqld_alive" ]; then
+    RES=`HOME=~zabbix mysql -h $SERVICEIP -P $SERVICEPORT -u$USERNAME -p$PASSWORD -N -e 'select 1 from dual;' 2>/dev/null`
+    if [ "$RES" = "1" ]; then
+        echo 1
+    else
+        echo 0
+    fi
+    exit
+elif [ "$ITEM" = "slave_running" ]; then
+    # Check for running slave
+    RES=`HOME=~zabbix mysql -h $SERVICEIP -P $SERVICEPORT -u$USERNAME -p$PASSWORD -e 'SHOW SLAVE STATUS\G' 2>/dev/null | egrep '(Slave_IO_Running|Slave_SQL_Running):' | awk -F: '{print $2}' | tr '\n' ','`
+    if [ "$RES" = " Yes, Yes," ]; then
+        echo 1
+    else
+        echo 0
+    fi
+    exit
+elif [ "$ITEM" = "check" ]; then
+    echo 1
+    $CMD 2>&1 > /dev/null
+fi
