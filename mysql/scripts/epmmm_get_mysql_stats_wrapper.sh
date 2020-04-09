3300e050ff3562d6f21bd594f755a5a514c2da55
earl86 <earl86@users.noreply.github.com>
2020-04-09 09:52:20 +0800

    modify

--- mysql/scripts/epmmm_get_mysql_stats_wrapper.sh
+++ mysql/scripts/epmmm_get_mysql_stats_wrapper.sh
@@ -9,20 +9,25 @@
 
 USERNAME=zabbix
 PASSWORD=zabbix
-ZABBIXSERVER=192.168.0.1
+ZABBIXSERVER=192.168.1.2
+ZABBIXSERVER_PORT=10051
+
+HOSTNAME=`hostname`
+if [ "${HOSTNAME:0:4}" = "xxx" ]; then
+   ZABBIXSERVER=192.168.1.3
+fi
 
 SERVICEHOSTNAME=$1
-SERVICEHOST=$2
+SERVICEIP=$2
 SERVICEPORT=$3
 ITEM=$4
 
 DIR=`dirname $0`
 
-CMD="/usr/bin/python $DIR/epmmm_get_mysql_stats.py --servicehostname $SERVICEHOSTNAME --servicehost $SERVICEHOST --serviceport $SERVICEPORT --username $USERNAME --password $PASSWORD"
-CACHEFILE="/tmp/$SERVICEHOST-$SERVICEPORT-epmmm-mysql_zabbix_stats.txt"
+CMD="/usr/bin/python $DIR/epmmm_get_mysql_stats.py --servicehostname $SERVICEHOSTNAME --serviceip $SERVICEIP --serviceport $SERVICEPORT --username $USERNAME --password $PASSWORD --zabbixserver $ZABBIXSERVER --zabbixserver_port $ZABBIXSERVER_PORT"
 
 if [ "$ITEM" = "mysqld_alive" ]; then
-    RES=`HOME=~zabbix mysql -h $SERVICEHOST -P $SERVICEPORT -u$USERNAME -p$PASSWORD -N -e 'select 1 from dual;' 2>/dev/null`
+    RES=`HOME=~zabbix mysql -h $SERVICEIP -P $SERVICEPORT -u$USERNAME -p$PASSWORD -N -e 'select 1 from dual;' 2>/dev/null`
     if [ "$RES" = "1" ]; then
         echo 1
     else
@@ -31,33 +36,20 @@ if [ "$ITEM" = "mysqld_alive" ]; then
     exit
 elif [ "$ITEM" = "slave_running" ]; then
     # Check for running slave
-    RES=`HOME=~zabbix mysql -h $SERVICEHOST -P $SERVICEPORT -u$USERNAME -p$PASSWORD -e 'SHOW SLAVE STATUS\G' 2>/dev/null | egrep '(Slave_IO_Running|Slave_SQL_Running):' | awk -F: '{print $2}' | tr '\n' ','`
+    RES=`HOME=~zabbix mysql -h $SERVICEIP -P $SERVICEPORT -u$USERNAME -p$PASSWORD -e 'SHOW SLAVE STATUS\G' 2>/dev/null | egrep '(Slave_IO_Running|Slave_SQL_Running):' | awk -F: '{print $2}' | tr '\n' ','`
     if [ "$RES" = " Yes, Yes," ]; then
         echo 1
     else
         echo 0
     fi
     exit
-elif [ -e $CACHEFILE ]; then
-    # Check and run the script
-    #TIMEFLM=`stat -c %Y /tmp/$SERVICEHOST-$SERVICEPORT-epmmm-mysql_zabbix_stats.txt`
-    echo 1
-    TIMEFLM=`stat -c %Y $CACHEFILE`
-    TIMENOW=`date +%s`
-    if [ `expr $TIMENOW - $TIMEFLM` -gt 30 ]; then
-        rm -f $CACHEFILE
-        $CMD 2>&1 > /dev/null
+elif [ "$ITEM" = "check" ]; then
+    $CMD
+    if [ $? = "0" ]; then
+    	echo 1
+    else
+    	echo 0
     fi
-else
-    echo 1
-    $CMD 2>&1 > /dev/null
-fi
-
-# Parse cache file
-if [ -e $CACHEFILE ]; then
-    zabbix_sender -z $ZABBIXSERVER -p 10051 -i $CACHEFILE 2>&1 > /dev/null 
-else
-    echo "ERROR: run the command manually to investigate the problem: $CMD"
 fi
 
 
