ab517ee939952540c83453d4f812794e1cd75a05
earl86 <earl86@users.noreply.github.com>
2021-07-22 17:59:01 +0800

    Add files via upload

--- tendis/epmmm_get_tendis_stats.py
+++ tendis/epmmm_get_tendis_stats.py
@@ -12,13 +12,15 @@ parser = argparse.ArgumentParser(description='Zabbix Tendis status script')
 parser.add_argument('-n','--servicename',dest='tendis_servicename',action='store',help='Tendis servicename be the same in zabbix hostname',default=None)
 parser.add_argument('-s','--serviceip',dest='tendis_serviceip',action='store',help='Tendis service ip',default=None)
 parser.add_argument('-p','--port',dest='tendis_port',action='store',help='Tendis server port',default=8901,type=int)
+parser.add_argument('-a','--auth',dest='tendis_password',action='store',help='Tendis server password',default=None)
 args = parser.parse_args()
 
 tendis_servicename = args.tendis_servicename
 tendis_serviceip = args.tendis_serviceip
 tendis_port = args.tendis_port
+tendis_password = args.tendis_password
 
-zabbix_host = '10.17.13.2'      # Zabbix Server IP
+zabbix_host = '192.168.1.108'      # Zabbix Server IP
 zabbix_port = 10051             # Zabbix Server Port
 
 
@@ -28,7 +30,7 @@ def send_to_zabbix(packet, zabbix_host, zabbix_port):
 
 
 def main():
-    client = redis.StrictRedis(host=tendis_serviceip, port=tendis_port)
+    client = redis.StrictRedis(host=tendis_serviceip, port=tendis_port, , password=tendis_password)
     server_info = client.info(section='all')
 
     packet = []
