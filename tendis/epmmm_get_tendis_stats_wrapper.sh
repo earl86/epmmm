17d5806f8a2885810ec4b2a7a0c07d128f8a42c8
earl86 <earl86@users.noreply.github.com>
2021-08-03 14:50:54 +0800

    Add files via upload

--- tendis/epmmm_get_tendis_stats_wrapper.sh
+++ tendis/epmmm_get_tendis_stats_wrapper.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 # The wrapper for tendis of zabbix python script.
 # It runs the script every 30 seconds. and parses the cache file on each following run.
-# Test the script using: zabbix_get -s servicename -p 10050 -k "epmmm.tendis.check[servicename,serviceip,8901,check]"
+# Test the script using: zabbix_get -s servicename -p 10050 -k "epmmm.tendis.check[servicename,serviceip,3901,servicepassword,check]"
 # The script need Python Running Environment pip3 install py-zabbix redis argparse
-# The default monitor tendis port is 8901, if need to monitor other port, you need to add the Macros:{$TENDIS_PORT}=xxxx on the monitor Host
+# The default monitor tendis port is 3901, if need to monitor other port, you need to add the Macros:{$TENDIS_PORT}=xxxx and {$TENDIS_PASSWORD}=xxxx on the monitor Host
 # Authors: earl86
 
 SERVICEHOSTNAME=$1
 SERVICEHOSTIP=$2
 SERVICEPORT=$3
-ITEM=$4
+SERVICEPASSWORD=$4
+ITEM=$5
 
 DIR=`dirname $0`
 
-CMD="/usr/bin/python3 $DIR/epmmm_get_tendis_stats.py -n $SERVICEHOSTNAME -s $SERVICEHOSTIP -p $SERVICEPORT"
+CMD="/usr/bin/python3 $DIR/epmmm_get_tendis_stats.py -n $SERVICEHOSTNAME -s $SERVICEHOSTIP -p $SERVICEPORT -a $SERVICEPASSWORD"
 
 if [ "$ITEM" = "check" ]; then
     $CMD 2>&1 > /dev/null
