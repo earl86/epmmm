692307a22ac5488d1db277ee56dec561b8eabadd
earl86 <earl86@users.noreply.github.com>
2022-02-25 10:54:24 +0800

    Add files via upload

--- numacpu/epmmm_get_numa_node_cpuinfo.py
+++ numacpu/epmmm_get_numa_node_cpuinfo.py
@@ -2,6 +2,9 @@
 #pip3 install strip_ansi
 #pip3 install pexpect
 
+import re
+import logging
+import time
 import pexpect
 import argparse
 from strip_ansi import strip_ansi
@@ -12,35 +15,44 @@ parser.add_argument('-n','--hostname',dest='hostname',action='store',help='hostn
 args = parser.parse_args()
 
 hostname = args.hostname
-zabbix_host = '192.168.1.100'      # Zabbix Server IP
+zabbix_host = '10.17.13.2'      # Zabbix Server IP
 zabbix_port = 10051             # Zabbix Server Port
 
+logging.basicConfig(level=logging.INFO,
+                    filename='/var/log/zabbix/epmmm-get-numa-node-cpuinfo.log',
+                    datefmt='%Y/%m/%d %H:%M:%S',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(module)s - %(message)s')
+
+logger = logging.getLogger(__name__)
 
 def send_to_zabbix(packet, zabbix_host, zabbix_port):
     server = ZabbixSender(zabbix_host,zabbix_port)
     server.send(packet)
 
 def generate_packet(hostname):
-    cmd = 'top -n 2'
-    child = pexpect.spawn(cmd, timeout=2, encoding='utf-8',env = {"TERM": "dumb"})
-    child.send('2')
-    info=child.read()
-    a=info.split('\n')
     packet = []
+    cmd = 'top -n 2 -d 10'
+    child = pexpect.spawn(cmd, timeout=10, encoding='utf-8',env = {"TERM": "dumb"})
+    child.send('2')
+    time.sleep(1)
+    info=child.read()
+    #logger.info('%s!', info)
+    child.close()
+    a=info.split('\n')
     for line in a:
         if (line.find("%Node") == 0):
-            #print(strip_ansi(line))
-            newline=strip_ansi(line).split()
-            cpunude=strip_ansi(newline[0]).strip('%')
-            packet.append(ZabbixMetric(hostname, "cpu.numa.us.[%s]" % cpunude,strip_ansi(newline[2])))
-            packet.append(ZabbixMetric(hostname, "cpu.numa.sy.[%s]" % cpunude,strip_ansi(newline[4])))
-            packet.append(ZabbixMetric(hostname, "cpu.numa.ni.[%s]" % cpunude,strip_ansi(newline[6])))
-            packet.append(ZabbixMetric(hostname, "cpu.numa.id.[%s]" % cpunude,strip_ansi(newline[8])))
-            packet.append(ZabbixMetric(hostname, "cpu.numa.wa.[%s]" % cpunude,strip_ansi(newline[10])))
-            packet.append(ZabbixMetric(hostname, "cpu.numa.hi.[%s]" % cpunude,strip_ansi(newline[12])))
-            packet.append(ZabbixMetric(hostname, "cpu.numa.si.[%s]" % cpunude,strip_ansi(newline[14])))
-            packet.append(ZabbixMetric(hostname, "cpu.numa.st.[%s]" % cpunude,strip_ansi(newline[16])))
-
+            newline =  re.split(r':|,', strip_ansi(line))
+            #logger.info('%s!', newline)
+            cpunode=newline[0].strip().strip('%')
+            packet.append(ZabbixMetric(hostname, "cpu.numa.us.[%s]" % cpunode,newline[1].strip().split()[0]))
+            packet.append(ZabbixMetric(hostname, "cpu.numa.sy.[%s]" % cpunode,newline[2].strip().split()[0]))
+            packet.append(ZabbixMetric(hostname, "cpu.numa.ni.[%s]" % cpunode,newline[3].strip().split()[0]))
+            packet.append(ZabbixMetric(hostname, "cpu.numa.id.[%s]" % cpunode,newline[4].strip().split()[0]))
+            packet.append(ZabbixMetric(hostname, "cpu.numa.wa.[%s]" % cpunode,newline[5].strip().split()[0]))
+            packet.append(ZabbixMetric(hostname, "cpu.numa.hi.[%s]" % cpunode,newline[6].strip().split()[0]))
+            packet.append(ZabbixMetric(hostname, "cpu.numa.si.[%s]" % cpunode,newline[7].strip().split()[0]))
+            packet.append(ZabbixMetric(hostname, "cpu.numa.st.[%s]" % cpunode,newline[8].strip().split()[0]))
+    #logger.info('%s!', packet)
     return packet
 
 def main():
